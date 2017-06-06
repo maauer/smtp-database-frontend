b3f422f3368a198d264a3e2920397c47ea32d921
max (menachem) <maauer@users.noreply.github.com>
2017-06-06 07:46:13 -0400

    Update mail.py

--- mail.py
+++ mail.py
@@ -1,8 +1,13 @@
+#http://pythoncentral.io/introduction-to-sqlite-in-python/?ref=EmailThis.me
+
 import sqlite3
 
 
 database_name = 'greeleysmtp.db'
 
 database = sqlite3.connect(database_name)
+
+cursor = db.cursor()
+
 
 database.close()
